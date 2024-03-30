ba3705c4f7053125df3e61db62a92aeb9906e5ba
KayChicken <105989236+KayChicken@users.noreply.github.com>
2024-03-30 11:45:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Random().Next(100));
+        }
+    }
+}
